fac8fee8e8074ca35cbd91abdb787e6d5fe65e08
lukasz.filiciak <lukasz.filiciak@farmaprom.pl>
2022-01-08 09:43:36 +0100

    Initial Commit

---
+++ Wojna/WojnaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WojnaApp.swift
+//  Wojna
+//
+//  Created by Lukasz Filiciak on 08/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct WojnaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
